23ed54e5735723194b5def419695a7c9dc1e200f
michael <sundong781@gmail.com>
2024-07-08 00:25:13 +1000

    update the cloud function code

--- cloudfunction/main.py
+++ cloudfunction/main.py
@@ -4,6 +4,8 @@ import requests
 import json
 from datetime import datetime
 from google.cloud import storage
+from google.cloud import dataflow_v1beta3
+from google.cloud.dataflow_v1beta3.types import LaunchTemplateParameters, RuntimeEnvironment
 
 def http_handler(request):
     api_key = "e2408ac071e218ec873f0a4ccde2e0fb"
@@ -31,6 +33,41 @@ def http_handler(request):
 
         # Upload to Cloud Storage
         blob.upload_from_string(json.dumps(data))
+
+
+        # Trigger Dataflow job
+        project_id = "easygo-task-428510"
+        job_name = f'weather-etl-{datetime.now().strftime("%Y%m%d-%H%M%S")}'
+        template_path = "gs://easygo-df-bucket/templates/weather-etl-job"
+        region = "australia-southeast1"
+
+        # Set up the client
+        client = dataflow_v1beta3.TemplatesServiceClient()
+
+        # Set up the runtime environment
+        runtime_environment = RuntimeEnvironment(
+            temp_location='gs://easygo-df-bucket/temp'
+        )
+
+        # Set up the launch parameters
+        launch_parameters = LaunchTemplateParameters(
+            job_name=job_name,
+            parameters={},  # Add any template parameters here
+            environment=runtime_environment
+        )
+
+        # Launch the template
+        response = client.launch_template(
+            request={
+                "project_id": project_id,
+                "gcs_path": template_path,
+                "launch_parameters": launch_parameters,
+                "location": region,
+            }
+        )
+
+
+        return f'Data ingested and Dataflow job triggered: {response.job.id}'
 
     except Exception as e:
         print(f"Error uploading to Cloud Storage: {e}")
